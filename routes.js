32eb4a1b538d59996584bbf29682093be96a7127
Jeffrey <jlevym@gmail.com>
2016-04-17 19:02:40 +0300

    update blog and routes with Funtime_v1 code

--- routes.js
+++ routes.js
@@ -1,9 +1,54 @@
 Router.configure({
   layoutTemplate: 'layout'
 });
+
 Router.route('/', function () {
-  this.render('home');
+  this.render('header', {
+    to:"navbar"
+  });
+  this.render('home', {
+    to:"main"
+  });
 });
+
+Router.route('/home1', function () { // not in use for Funtime
+  this.render('home1', {
+    to:"main"
+  });
+});
+
 Router.route('/news', function () {
-  this.render('news');
+  this.render('header', {
+    to:"navbar"
+  });
+  this.render('news', {
+    to:"main"
+  });
+});
+
+Router.route('/kids', function () { // not in use for Funtime
+  this.render('header', {
+    to:"navbar"
+  });
+  this.render('kids', {
+    to:"main"
+  });
+});
+
+Router.route('/this_year', function () {
+  this.render('header', {
+    to:"navbar"
+  });
+  this.render('this_year', {
+    to:"main"
+  });
+});
+
+Router.route('/blog', function () {
+  this.render('header', {
+    to:"navbar"
+  });
+  this.render('myBlogIndexTemplate', {
+    to:"main"
+  });
 });
